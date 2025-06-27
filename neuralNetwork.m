8b3aa401f367e90d513c1e9a16016b21d4962ba7
Nambiar, Adithye Sajesh <adithye.nambiar@stud.uni-due.de>
2025-06-27 06:07:03 +0200

    Everything

--- neuralNetwork.m
+++ neuralNetwork.m
@@ -9,19 +9,15 @@ classdef neuralNetwork
         lr;     % learning rate
         w_ih;   % link weights between input layer and hidden layer
         w_ho;   % link weights between hidden layer and output layer
-<<<<<<< HEAD
         %extra
         b_ih;
         b_ho;
         lr_decay=0.85;
         %extra
-=======
->>>>>>> 97b2190e9861b5356ab50a3cb8371ba60a705bd4
     end
 
     methods
         function self = neuralNetwork(inputnodes, hiddennodes, outputnodes, learningrate)
-<<<<<<< HEAD
             self.inodes=inputnodes; 
             self.hnodes=hiddennodes;
             self.onodes=outputnodes;
@@ -32,16 +28,10 @@ classdef neuralNetwork
            self.b_ih=zeros(self.hnodes,1);
            self.b_ho=zeros(self.onodes,1);
            %extra
-=======
-            % neuralNetwork: Construct an instance of this class
-            % Your code goes here. 
-            
->>>>>>> 97b2190e9861b5356ab50a3cb8371ba60a705bd4
         end
             
         % Query the neural network. The function takes a vector of inputs
         % and computes the outputs, i.e. a "prediction".
-<<<<<<< HEAD
         function [final_outputs,hidden_output] = query(self, inputs)
             hidden_output=self.w_ih * (inputs) + self.b_ih; %extra
             hidden_output = self.actfun(hidden_output);
@@ -49,16 +39,11 @@ classdef neuralNetwork
             final_outputs = self.actfun(final_outputs);
             
             
-=======
-        function final_outputs = query(self, inputs)
-            % your code goes here.
->>>>>>> 97b2190e9861b5356ab50a3cb8371ba60a705bd4
         end
 
         % Train the neural network. This function learns the link weights
         % from the training data.
         function self = train(self, inputs, targets)
-<<<<<<< HEAD
             %error calculation
            [final,hidden]=self.query(inputs);
            output_error=targets-final;
@@ -85,25 +70,14 @@ classdef neuralNetwork
             [~,guessed_number]=max(final);
             correctness =(target_number==guessed_number);
                
-=======
-            % your code goes here
->>>>>>> 97b2190e9861b5356ab50a3cb8371ba60a705bd4
         end
     
     end
 
     methods(Static)
         function out = actfun(x)
-<<<<<<< HEAD
             out=1./(1+exp(-x));
         end
     end
 
 end
-=======
-            % The code for the activation function goes here.
-        end
-    end
-
-end
->>>>>>> 97b2190e9861b5356ab50a3cb8371ba60a705bd4
